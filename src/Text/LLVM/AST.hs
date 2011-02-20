41318f9934c40a8bfef4aaff92f118ddaebe7a91
Trevor Elliott <trevor@galois.com>
2011-02-19 23:55:40 -0800

    Deal with more AST

--- src/Text/LLVM/AST.hs
+++ src/Text/LLVM/AST.hs
@@ -7,10 +7,17 @@ import Text.PrettyPrint.HughesPJ
 commas :: [Doc] -> Doc
 commas ds = hcat (intersperse (comma <> space) ds)
 
+int32 :: Int32 -> Doc
+int32  = integer . fromIntegral
+
+angles :: Doc -> Doc
+angles d = char '<' <> d <> char '>'
+
 -- Modules ---------------------------------------------------------------------
 
 data Module = Module
   { modTypes    :: [TypeDecl]
+  , modGlobals  :: [Global]
   , modDeclares :: [Declare]
   , modDefines  :: [Define]
   } deriving (Show)
@@ -18,15 +25,17 @@ data Module = Module
 emptyModule :: Module
 emptyModule  = Module
   { modTypes    = []
+  , modGlobals  = []
   , modDeclares = []
   , modDefines  = []
   }
 
 ppModule :: Module -> Doc
-ppModule m = hcat $ concat [ map ppTypeDecl (modTypes m)
-                           , map ppDeclare (modDeclares m)
-                           , map ppDefine (modDefines m)
-                           ]
+ppModule m = hcat $ concat
+  [ map ppTypeDecl (modTypes m)
+  , map ppDeclare (modDeclares m)
+  , map ppDefine (modDefines m)
+  ]
 
 -- Identifiers -----------------------------------------------------------------
 
@@ -46,26 +55,66 @@ ppSymbol (Symbol n) = char '@' <> text n
 
 -- Types -----------------------------------------------------------------------
 
-data Type
-  = Void
-  | Alias Ident
+data PrimType
+  = Label
+  | Void
   | Integer Int32
-  | Float
+  | FloatType FloatType
+  | X86mmx
+  | Metadata
+    deriving (Show)
+
+ppPrimType :: PrimType -> Doc
+ppPrimType Label          = text "label"
+ppPrimType Void           = text "void"
+ppPrimType (Integer i)    = char 'i' <> integer (toInteger i)
+ppPrimType (FloatType ft) = ppFloatType ft
+ppPrimType X86mmx         = text "x86mmx"
+ppPrimType Metadata       = text "metadata"
+
+data FloatType
+  = Float
   | Double
-  | PtrTo Type
-  | Label
+  | Fp128
+  | X86_fp80
+  | PPC_fp128
+    deriving (Show)
+
+ppFloatType :: FloatType -> Doc
+ppFloatType Float     = text "float"
+ppFloatType Double    = text "double"
+ppFloatType Fp128     = text "fp128"
+ppFloatType X86_fp80  = text "x86_fp80"
+ppFloatType PPC_fp128 = text "ppc_fp128"
+
+data Type
+  = PrimType PrimType
+  | Alias Ident
+  | Array Int32 Type
   | FunTy [Type] Type
+  | PtrTo Type
+  | Struct [Type]
+  | PackedStruct [Type]
+  | Vector Int32 PrimType
+  | Opaque
     deriving (Show)
 
 ppType :: Type -> Doc
-ppType Void         = text "void"
-ppType (Alias i)    = ppIdent i
-ppType (Integer i)  = char 'i' <> integer (toInteger i)
-ppType Float        = text "float"
-ppType Double       = text "double"
-ppType (PtrTo ty)   = ppType ty <> char '*'
-ppType Label        = text "label"
-ppType (FunTy as r) = ppType r <> parens (commas (map ppType as))
+ppType (PrimType pt)     = ppPrimType pt
+ppType (Alias i)         = ppIdent i
+ppType (Array len ty)    = brackets (int32 len <+> char 'x' <+> ppType ty)
+ppType (PtrTo ty)        = ppType ty <> char '*'
+ppType (Struct ts)       = braces (commas (map ppType ts))
+ppType (PackedStruct ts) = angles (braces (commas (map ppType ts)))
+ppType (FunTy as r)      = ppType r <> parens (commas (map ppType as))
+ppType (Vector len pt)   = angles (int32 len <+> char 'x' <+> ppPrimType pt)
+ppType Opaque            = text "opaque"
+
+iTy :: Int32 -> Type
+iTy  = PrimType . Integer
+
+floatTy :: FloatType -> Type
+floatTy  = PrimType . FloatType
 
 -- Top-level Type Aliases ------------------------------------------------------
 
@@ -75,7 +124,20 @@ data TypeDecl = TypeDecl
   } deriving (Show)
 
 ppTypeDecl :: TypeDecl -> Doc
-ppTypeDecl td = ppIdent (typeName td) <+> char '=' <+> ppType (typeValue td)
+ppTypeDecl td = ppIdent (typeName td) <+> char '='
+            <+> text "type" <+> ppType (typeValue td)
+
+-- Globals ---------------------------------------------------------------------
+
+data Global = Global
+  { globalSym   :: Symbol
+  , globalType  :: Type
+  , globalValue :: Value
+  } deriving Show
+
+ppGlobal :: Global -> Doc
+ppGlobal g = ppSymbol (globalSym g) <+> char '=' <+> text "global"
+         <+> ppType (globalType g) <+> ppValue (globalValue g)
 
 -- Declarations ----------------------------------------------------------------
 
@@ -179,11 +241,17 @@ call  = Call False
 tailCall :: Type -> Symbol -> [Arg] -> Instr
 tailCall  = Call True
 
-add :: Type -> Value -> Value -> Instr
-add ty l r = GenInstr "add" [TypedArg (Typed ty l),UntypedArg r]
+add :: Typed Value -> Value -> Instr
+add l r = GenInstr "add" [TypedArg l,UntypedArg r]
 
-sub :: Type -> Value -> Value -> Instr
-sub ty l r = GenInstr "sub" [TypedArg (Typed ty l),UntypedArg r]
+sub :: Typed Value -> Value -> Instr
+sub l r = GenInstr "sub" [TypedArg l,UntypedArg r]
 
-mul :: Type -> Value -> Value -> Instr
-mul ty l r = GenInstr "mul" [TypedArg (Typed ty l),UntypedArg r]
+mul :: Typed Value -> Value -> Instr
+mul l r = GenInstr "mul" [TypedArg l,UntypedArg r]
+
+getelementptr :: Typed Value -> [(Type,Value)] -> Instr
+getelementptr tv ixs = GenInstr "getelementptr" (TypedArg tv : args)
+  where
+  args | null ixs  = [TypedArg (Typed (PrimType (Integer 32)) (ValNum 0))]
+       | otherwise = map (TypedArg . uncurry Typed) ixs
