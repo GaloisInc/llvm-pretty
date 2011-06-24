f979e5e7b668e20fadd859b75bd96de0889b87e0
Trevor Elliott <trevor@galois.com>
2011-06-24 00:01:18 -0700

    Add an example

---
+++ examples/Factorial.hs
@@ -0,0 +1,28 @@
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE DoRec #-}
+
+module Factorial where
+
+import Text.LLVM
+import Text.LLVM.AST
+
+factorial :: Module
+factorial  = snd $ runLLVM $ do
+  define emptyFunAttrs (iT 32) "factorial" (iT 32) $ \ x -> do
+    "entry"
+    jump "test"
+
+    rec "test"
+        i   <- phi (iT 32) [x     `from` "entry", i'   `from` "incr"]
+        acc <- phi (iT 32) [int 1 `from` "entry", acc' `from` "incr"]
+
+        b   <- icmp Iule i (int 1)
+        br b "exit" "incr"
+
+        "incr"
+        acc' <- mul acc i
+        i'   <- sub i (int 1)
+        jump "test"
+
+    "exit"
+    ret acc
