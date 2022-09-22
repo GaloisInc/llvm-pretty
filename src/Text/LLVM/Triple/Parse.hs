65c628e4fdcb26a4f002f85a9179f6a149e84fd7
Langston Barrett <langston.barrett@gmail.com>
2022-09-22 14:25:01 -0400

    Add a Haddock for parseVendor

--- src/Text/LLVM/Triple/Parse.hs
+++ src/Text/LLVM/Triple/Parse.hs
@@ -18,5 +18,8 @@ import qualified Text.LLVM.Triple.Print as Print
 enumTable :: Bounded a => Enum a => (a -> String) -> [(String, a)]
 enumTable prnt = [(prnt a, a) | a <- enumFrom minBound]
 
+-- | @llvm::parseVendor@
+--
+-- https://github.com/llvm/llvm-project/blob/llvmorg-15.0.1/llvm/lib/Support/Triple.cpp#L529
 parseVendor :: String -> Vendor
 parseVendor s = fromMaybe UnknownVendor (lookup s (enumTable Print.vendorName))
