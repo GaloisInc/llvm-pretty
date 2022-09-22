80ab2bc069122cdd9b6ca22b9fbf03110776d509
Langston Barrett <langston.barrett@gmail.com>
2022-09-22 14:47:37 -0400

    Triple.Parse: Refactor, use a newtype for LookupTable

--- src/Text/LLVM/Triple/Parse.hs
+++ src/Text/LLVM/Triple/Parse.hs
@@ -4,6 +4,8 @@ Description : Parsing of LLVM target triples.
 License     : BSD3
 Maintainer  : Langston Barrett
 Stability   : experimental
+
+The declarations appear in this module in the same order as in the LLVM source.
 -}
 
 module Text.LLVM.Triple.Parse
@@ -17,26 +19,35 @@ import qualified Data.List as List
 import Text.LLVM.Triple.AST
 import qualified Text.LLVM.Triple.Print as Print
 
+newtype LookupTable a = LookupTable { getLookupTable :: [(String, a)] }
+
 -- | Helper, not exported
-enumTable :: Bounded a => Enum a => (a -> String) -> [(String, a)]
-enumTable prnt = [(prnt a, a) | a <- enumFrom minBound]
+enumLookupTable :: Bounded a => Enum a => (a -> String) -> LookupTable a
+enumLookupTable prnt = LookupTable [(prnt a, a) | a <- enumFrom minBound]
 
 -- | Helper, not exported.
 --
--- @lookupBy (==) == lookup@.
-lookupBy :: (a -> Bool) -> [(a, b)] -> Maybe b
-lookupBy p = Maybe.listToMaybe . map snd . filter (p . fst)
+-- @lookupBy (== s) == lookup s . getLookupTable@.
+lookupBy :: (String -> Bool) -> LookupTable a -> Maybe a
+lookupBy p = Maybe.listToMaybe . map snd . filter (p . fst) . getLookupTable
+
+lookupWithDefault :: LookupTable a -> a -> String -> a
+lookupWithDefault table def val = Maybe.fromMaybe def (lookupBy (== val) table)
+
+lookupByPrefixWithDefault :: LookupTable a -> a -> String -> a
+lookupByPrefixWithDefault table def pfx =
+  Maybe.fromMaybe def (lookupBy (pfx `List.isPrefixOf`) table)
 
 -- | @llvm::parseVendor@
 --
 -- https://github.com/llvm/llvm-project/blob/llvmorg-15.0.1/llvm/lib/Support/Triple.cpp#L529
 parseVendor :: String -> Vendor
-parseVendor s = Maybe.fromMaybe UnknownVendor (lookup s table)
-  where table = enumTable Print.vendorName
+parseVendor = lookupWithDefault table UnknownVendor
+  where table = enumLookupTable Print.vendorName
 
 -- | @llvm::parseOS@
 --
 -- https://github.com/llvm/llvm-project/blob/llvmorg-15.0.1/llvm/lib/Support/Triple.cpp#L549
 parseOS :: String -> OS
-parseOS s = Maybe.fromMaybe UnknownOS (lookupBy (`List.isPrefixOf` s) table)
-  where table = enumTable Print.osName
+parseOS = lookupByPrefixWithDefault table UnknownOS
+  where table = enumLookupTable Print.osName
