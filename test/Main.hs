47141b1c3ecba9e92d42a06c280c2802ea4c56c7
Kevin Quick <kquick@galois.com>
2023-08-03 11:14:13 -0700

    Add some initial pretty-output verification tests.

--- test/Main.hs
+++ test/Main.hs
@@ -3,6 +3,11 @@ module Main (main) where
 import qualified Test.Tasty as Tasty
 
 import qualified Triple
+import qualified Output
 
 main :: IO ()
-main = Tasty.defaultMain Triple.tests
+main = Tasty.defaultMain $ Tasty.testGroup "LLVM tests"
+       [
+         Output.tests
+       , Triple.tests
+       ]
