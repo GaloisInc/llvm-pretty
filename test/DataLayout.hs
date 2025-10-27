40d74ed175e3fb32ec5f33820f2c6b0eab6bb889
Kevin Quick <kquick@galois.com>
2025-10-27 09:01:04 -0700

    Remove redundant parentheses in DataLayout test.
    Co-authored-by: Ryan Scott <rscott@galois.com>

--- test/DataLayout.hs
+++ test/DataLayout.hs
@@ -11,21 +11,21 @@ tests =
   let rspDL s = Just $ "target datalayout = \"" <> s <> "\""
       ppDL = show . ppLLVM llvmVlatest ppDataLayout
   in testGroup "Test DataLayout"
-     [ testCase "datalayout 0" $ ppDL <$> (parseDataLayout "") @?= Just ""
+     [ testCase "datalayout 0" $ ppDL <$> parseDataLayout "" @?= Just ""
      , testCase "datalayout 1"
-       $ let dl = "e" in ppDL <$> (parseDataLayout dl) @?= rspDL dl
+       $ let dl = "e" in ppDL <$> parseDataLayout dl @?= rspDL dl
      , testCase "datalayout 2"
-       $ let dl = "e-m:e" in ppDL <$> (parseDataLayout dl) @?= rspDL dl
+       $ let dl = "e-m:e" in ppDL <$> parseDataLayout dl @?= rspDL dl
      , testCase "datalayout 3"
        $ let dl = "e-m:e-p270:32:32"
-         in ppDL <$> (parseDataLayout dl) @?= rspDL dl
+         in ppDL <$> parseDataLayout dl @?= rspDL dl
      , testCase "datalayout 4"
        $ let dl = "e-m:e-p270:32:32-p271:32:32-p272:64:64-i64:64-i128:128-f80:128-S128"
-         in ppDL <$> (parseDataLayout dl) @?= rspDL dl
+         in ppDL <$> parseDataLayout dl @?= rspDL dl
      , testCase "datalayout 5"
        $ let dl = "e-m:e-p270:32:32-p271:32:32-p272:64:64-i64:64-i128:128-f80:128-n8:16:32:64-S128"
-         in ppDL <$> (parseDataLayout dl) @?= rspDL dl
+         in ppDL <$> parseDataLayout dl @?= rspDL dl
      , testCase "datalayout 6"
        $ let dl = "e-m:e-p270:32:32-p271:32:32-p272:64:64-i64:64-i128:128-f80:128-n8:16:32:64-ni:3:4-S128"
-         in ppDL <$> (parseDataLayout dl) @?= rspDL dl
+         in ppDL <$> parseDataLayout dl @?= rspDL dl
      ]
